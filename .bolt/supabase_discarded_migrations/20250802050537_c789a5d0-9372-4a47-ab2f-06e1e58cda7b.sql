caaf497ec7124e5d5bbd9032b8c9a2a2477b4a93
m4milaad <mb4milad.bhattt@gmail.com>
2025-08-03 22:30:20 +0530

    hello

--- .bolt/supabase_discarded_migrations/20250802050537_c789a5d0-9372-4a47-ab2f-06e1e58cda7b.sql
+++
@@ -1,6 +0,0 @@
--- Update with properly generated bcrypt hashes
-UPDATE public.login_tbl SET password = '$2b$12$8CfvSVV9aRBjM9qJE3PEXeiVzJ7w9gkqd3pL5QkV7X8WJ3mL6BcH.' WHERE username = 'admin' AND type = 'Admin';
-UPDATE public.login_tbl SET password = '$2b$12$J8dOw5r6tLBvAHKQN7EW4O8gF3rLwG2zxX9mK5QpW7nE1uT6SxYvC' WHERE username = 'm4milaad' AND type = 'Admin';
-
--- Verify data exists
-SELECT username, LEFT(password, 10) as password_start, type FROM public.login_tbl WHERE type = 'Admin';
