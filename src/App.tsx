84a556e3a71f3f8e3a27a16c05495fd6328e5769
m4milaad <mb4milad.bhattt@gmail.com>
2025-11-07 15:06:26 +0530

    added speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -90,6 +91,7 @@ const App = () => {
         </TooltipProvider>
       </QueryClientProvider>
       <Analytics />
+      <SpeedInsights/>
     </ThemeProvider>
 
   );
